782bd46af4f95678b87def3e76b73d0ff61690bb
Santiago Chausis <u523982@teco.com.ar>
2023-01-24 15:51:51 -0300

    add filters and UI

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
 const pokemonContainer = document.querySelector('.pokemon-container')
+const filterButtons = document.querySelectorAll('.filter-button');
+const message = document.querySelector('.message');
 
+// Call the API
 async function fetchPokemon(id) {
     try {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    if (!res.ok) {
+        if (!res.ok) {
             throw new Error(`Error fetching Pokemon with id ${id}`);
         }
         const data = await res.json();
@@ -13,12 +16,50 @@ async function fetchPokemon(id) {
     }
 }
 
+// Call the pokemon
 async function fetchPokemons() {
-    for (let i = 550; i <= 650; i++) {
+    for (let i = 1; i <= 150; i++) {
         await fetchPokemon(i)
     }
+
+    //Filters
+    filterButtons.forEach(button => {
+        button.addEventListener('click', event => {
+            const selectedType = event.target.dataset.type;
+            const pokemonBlocks = document.querySelectorAll('.pokemon-block');
+            pokemonBlocks.forEach(block => {
+                block.style.display = 'none';
+            });
+            if (selectedType === '') {
+
+                pokemonBlocks.forEach(block => {
+                    block.style.display = 'block';
+                    message.textContent = 'Any pokemon available';
+                });
+            } else {
+                pokemonBlocks.forEach(block => {
+                    const types = block.querySelectorAll('.type');
+                    for (let i = 0; i < types.length; i++) {
+                        if (types[i].textContent.toLowerCase() === selectedType) {
+                            block.style.display = 'block';
+                            break;
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    const resetButton = document.querySelector('.reset-button');
+    resetButton.addEventListener('click', event => {
+        const pokemonBlocks = document.querySelectorAll('.pokemon-block');
+        pokemonBlocks.forEach(block => {
+            block.style.display = 'block';
+        });
+    });
 }
 
+// Create pokemon card
 const createPokemon = pokemon => {
     const card = document.createElement('div')
     card.classList.add('pokemon-block')
@@ -29,18 +70,26 @@ const createPokemon = pokemon => {
     const sprite = document.createElement('img');
     sprite.src = `https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`
     sprite.alt = `This is ${pokemon.name}`
-
+    sprite.loading = 'lazy'
+    sprite.className = 'img-detail'
     spriteContainer.appendChild(sprite)
+
+    const infoContainer = document.createElement('div')
+    infoContainer.classList.add('info-container')
 
     const number = document.createElement('p')
     number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
+    number.classList.add('pokemon-number')
 
     const name = document.createElement('p')
     name.classList.add('name')
     name.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    infoContainer.appendChild(name)
 
     const typesContainer = document.createElement('div')
     typesContainer.classList.add('types');
+    infoContainer.appendChild(typesContainer);
+
     pokemon.types.forEach((type, index) => {
         const typeElem = document.createElement('span');
         typeElem.classList.add('type');
@@ -52,26 +101,10 @@ const createPokemon = pokemon => {
         }
     });
 
-    card.appendChild(spriteContainer)
     card.appendChild(number)
-    card.appendChild(name)
-    card.appendChild(typesContainer);
-
+    card.appendChild(spriteContainer)
+    card.appendChild(infoContainer)
     pokemonContainer.appendChild(card)
 }
-fetchPokemons(1)
 
-const searchButton = document.querySelector("#search-button");
-searchButton.addEventListener("click", searchPokemon);
-
-async function searchPokemon() {
-    const searchInput = document.querySelector("#search-input").value;
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchInput}`);
-    const data = await res.json();
-
-    // Clear previous search results
-    pokemonContainer.innerHTML = "";
-
-    // Create and append the new search result
-    createPokemon(data);
-}
+fetchPokemons()
