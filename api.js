6db1cb350e489fca45a42ce4a171429aac4a37a1
Santiago Chausis <u523982@teco.com.ar>
2023-01-26 19:08:25 -0300

    Fix code and filters, new message of error

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ async function fetchPokemon(id) {
             throw new Error(`Error fetching Pokemon with id ${id}`);
         }
         const data = await res.json();
-        createPokemon(data);
+        return data;
     } catch (error) {
         console.error(error);
     }
@@ -18,93 +18,72 @@ async function fetchPokemon(id) {
 
 // Call the pokemon
 async function fetchPokemons() {
-    for (let i = 1; i <= 150; i++) {
-        await fetchPokemon(i)
+    try {
+        const promises = Array.from({ length: 10 }, (_, i) => fetchPokemon(i + 1));
+        const pokemonData = await Promise.all(promises);
+        pokemonData.forEach(createPokemon);
+    } catch (error) {
+        console.error(error);
     }
-
-    //Filters
-    filterButtons.forEach(button => {
-        button.addEventListener('click', event => {
-            const selectedType = event.target.dataset.type;
-            const pokemonBlocks = document.querySelectorAll('.pokemon-block');
-            pokemonBlocks.forEach(block => {
-                block.style.display = 'none';
-            });
-            if (selectedType === '') {
-
-                pokemonBlocks.forEach(block => {
-                    block.style.display = 'block';
-                    message.textContent = 'Any pokemon available';
-                });
-            } else {
-                pokemonBlocks.forEach(block => {
-                    const types = block.querySelectorAll('.type');
-                    for (let i = 0; i < types.length; i++) {
-                        if (types[i].textContent.toLowerCase() === selectedType) {
-                            block.style.display = 'block';
-                            break;
-                        }
-                    }
-                });
-            }
-        });
-    });
-
-    const resetButton = document.querySelector('.reset-button');
-    resetButton.addEventListener('click', event => {
-        const pokemonBlocks = document.querySelectorAll('.pokemon-block');
-        pokemonBlocks.forEach(block => {
-            block.style.display = 'block';
-        });
-    });
 }
+
+//Filters
+let visiblePokemon = 0;
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        const selectedType = event.target.dataset.type;
+        const pokemonBlocks = document.querySelectorAll('.pokemon-block');
+        visiblePokemon = 0;
+        if (selectedType === '') {
+            pokemonBlocks.forEach(block => {
+                block.style.display = 'block';
+                visiblePokemon++;
+            });
+        } else {
+            pokemonBlocks.forEach(block => {
+                const types = block.querySelectorAll('.type');
+                const isCorrectType = Array.from(types).some(type => type.textContent.toLowerCase() === selectedType);
+                if (isCorrectType) {
+                    block.style.display = 'block';
+                    visiblePokemon++;
+                } else {
+                    block.style.display = 'none';
+                }
+            });
+        }
+        if (visiblePokemon === 0) {
+            message.textContent = 'Any pokemon available';
+        } else {
+            message.textContent = '';
+        }
+    });
+});
+
 
 // Create pokemon card
 const createPokemon = pokemon => {
     const card = document.createElement('div')
     card.classList.add('pokemon-block')
 
-    const spriteContainer = document.createElement('div')
-    spriteContainer.classList.add('img-container');
+    const types = pokemon.types.map(type => `
+        <span class="type type-${type.type.name}">${type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</span>
+    `).join(" ");
 
-    const sprite = document.createElement('img');
-    sprite.src = `https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`
-    sprite.alt = `This is ${pokemon.name}`
-    sprite.loading = 'lazy'
-    sprite.className = 'img-detail'
-    spriteContainer.appendChild(sprite)
-
-    const infoContainer = document.createElement('div')
-    infoContainer.classList.add('info-container')
-
-    const number = document.createElement('p')
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`
-    number.classList.add('pokemon-number')
-
-    const name = document.createElement('p')
-    name.classList.add('name')
-    name.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    infoContainer.appendChild(name)
-
-    const typesContainer = document.createElement('div')
-    typesContainer.classList.add('types');
-    infoContainer.appendChild(typesContainer);
-
-    pokemon.types.forEach((type, index) => {
-        const typeElem = document.createElement('span');
-        typeElem.classList.add('type');
-        typeElem.classList.add(`type-${type.type.name}`);
-        typeElem.textContent = type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1);
-        typesContainer.appendChild(typeElem);
-        if (index !== pokemon.types.length - 1) {
-            typesContainer.appendChild(document.createTextNode(' '));
-        }
-    });
-
-    card.appendChild(number)
-    card.appendChild(spriteContainer)
-    card.appendChild(infoContainer)
-    pokemonContainer.appendChild(card)
-}
+    const html = `
+        <div class="img-container">
+            <img src="https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg" alt="This is ${pokemon.name}" loading="lazy" class="img-detail">
+        </div>
+        <div class="info-container">
+            <p class="pokemon-number">#${pokemon.id.toString().padStart(3, 0)}</p>
+            <p class="name">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
+            <div class="types">
+                ${types}
+            </div>
+        </div>
+    `
+    card.innerHTML = html;
+    pokemonContainer.appendChild(card);
+};
 
 fetchPokemons()
